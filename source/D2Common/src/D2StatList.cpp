27566a6f66879cc8b6f0561d6e4a4c9246368989
Lectem <lectem@gmail.com>
2021-01-16 23:51:33 +0100

    Fix sub_6FDB5830 returning value with optype == 1 instead of additionning

--- source/D2Common/src/D2StatList.cpp
+++ source/D2Common/src/D2StatList.cpp
@@ -218,7 +218,7 @@ int __fastcall sub_6FDB5830(D2StatListExStrc* pStatListEx, int nLayer_StatId)
 
 				if (nPreviousOpStatValue)
 				{
-					return ComputeStatPercentage(nPreviousOpStatValue, pStatListEx, opStatData.nStat);
+					nAccumulatedValue += ComputeStatPercentage(nPreviousOpStatValue, pStatListEx, opStatData.nStat);
 				}
 			}
 			break;
