806109e911530e1b0ed67cd5f4a67467a1018de0
Lectem <lectem@gmail.com>
2021-07-03 17:58:30 +0200

    Prepare D2Common.0x6FDAA9F0, but mark as not implemented

--- source/D2Common/include/Path/PathMisc.h
+++ source/D2Common/include/Path/PathMisc.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "CommonDefinitions.h"
-
+#include "Path.h"
 #pragma pack(1)
 
 #pragma pack()
 
 int __fastcall sub_6FDAB750(int nX1, int nY1, int nX2, int nY2);
+//D2Common.0x6FDAA9F0
+int __fastcall sub_6FDAA9F0(D2PathInfoStrc* pPathInfo);
