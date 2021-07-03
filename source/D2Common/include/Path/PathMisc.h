1dc570748d91298acfe7c8a9f0a5b796dc5f741b
Lectem <lectem@gmail.com>
2021-07-03 20:07:23 +0200

    Add declarations for a few PATH functions

--- source/D2Common/include/Path/PathMisc.h
+++ source/D2Common/include/Path/PathMisc.h
@@ -6,7 +6,12 @@
 
 #pragma pack()
 
+//D2Common.0x6FDABA50
+int __stdcall  sub_6FDABA50(D2PathPointStrc pPoint1, D2PathPointStrc pPoint2);
+//D2Common.0x6FDAB750
 int __fastcall sub_6FDAB750(int nX1, int nY1, int nX2, int nY2);
+//D2Common.0x6FDAB0B0
+int __fastcall sub_6FDAB0B0(D2PathInfoStrc* pPathInfo);
 //D2Common.0x6FDAA9F0
 int __fastcall sub_6FDAA9F0(D2PathInfoStrc* pPathInfo);
 //D2Common.0x6FDAB130
@@ -15,3 +20,9 @@ int __fastcall sub_6FDAB130(D2PathInfoStrc* pPathInfo);
 int __fastcall sub_6FDAB270(D2PathInfoStrc* pPathInfo);
 //D2Common.0x6FDAB1E0
 int __fastcall sub_6FDAB1E0(D2PathInfoStrc* pPathInfo);
+//D2Common.0x6FDAB240
+int __fastcall sub_6FDAB240(D2PathInfoStrc* pPathInfo);
+//D2Common.0x6FDAB0C0
+int __fastcall sub_6FDAB0C0(D2PathInfoStrc* pPathInfo);
+//D2Common.0x6FDAC270
+int __fastcall sub_6FDAC270(D2PathInfoStrc* pPathInfo);
