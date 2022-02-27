6627e21490122922d735099670cf7462d6842ed7
Mir Drualga <IAmRealTrial@gmail.com>
2022-02-26 16:45:21 -0800

    Add Unicode::strlen test

--- source/D2Lang/tests/D2LangTests.cpp
+++ source/D2Lang/tests/D2LangTests.cpp
@@ -3,10 +3,10 @@
 
 #include <D2Unicode.h>
 
-TEST_CASE("Unicode::strncmp")
-{
 #define D2_USTR(widestr) (const Unicode*)widestr
 
+TEST_CASE("Unicode::strncmp")
+{
     const size_t strLenDiablo2 = wcslen(L"Diablo2");
 
     SUBCASE("Ordering")
@@ -28,13 +28,31 @@ TEST_CASE("Unicode::strncmp")
         CHECK(Unicode::strncmp(D2_USTR(L"Diablo2"), D2_USTR(L"Diablo3"), strLenDiablo2 - 1) == 0);
         CHECK(Unicode::strncmp(D2_USTR(L"Diablo2"), D2_USTR(L"Diablo3"), strLenDiablo2) != 0);
     }
-	SUBCASE("Original game bug")
-	{
+    SUBCASE("Original game bug")
+    {
         // Any string that is the prefix of the other is accepted.
         CHECK(Unicode::strncmp(D2_USTR(L"Diablo"), D2_USTR(L"Diablo2"),strLenDiablo2) == 0);
         CHECK(Unicode::strncmp(D2_USTR(L"Diablo2"), D2_USTR(L"Diablo"),strLenDiablo2) == 0);
 
         CHECK(Unicode::strncmp(D2_USTR(L""), D2_USTR(L"Diablo2"), strLenDiablo2) == 0);
         CHECK(Unicode::strncmp(D2_USTR(L"Diablo2"), D2_USTR(L""), strLenDiablo2) == 0);
-	}
+    }
+}
+
+TEST_CASE("Unicode::strlen")
+{
+    SUBCASE("Empty")
+    {
+        CHECK(Unicode::strlen(D2_USTR(L"")) == 0);
+    }
+    SUBCASE("Nonempty")
+    {
+        CHECK(Unicode::strlen(D2_USTR(L"Diablo2")) == 7);
+        CHECK(Unicode::strlen(D2_USTR(L"Mephisto")) == 8);
+        CHECK(Unicode::strlen(D2_USTR(L"Baal")) == 4);
+    }
+    SUBCASE("Null")
+    {
+        CHECK(Unicode::strlen(nullptr) == 0);
+    }
 }
