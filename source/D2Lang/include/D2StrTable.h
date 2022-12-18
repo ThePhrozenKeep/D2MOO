4d6812e28f3395715c2ab0973e0b0863aa7c3184
Mir Drualga <IAmRealTrial@gmail.com>
2022-12-17 16:26:15 -0800

    Implement STRTABLE_GetLangauge
    This gets the language from the archive files.

---
+++ source/D2Lang/include/D2StrTable.h
@@ -0,0 +1,43 @@
+/**
+ * D2MOO
+ * Copyright (c) 2020-2022  The Phrozen Keep community
+ *
+ * This file belongs to D2MOO.
+ * File authors: Mir Drualga
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <D2Constants.h>
+
+#include "D2Lang.h"
+
+/**
+ * Returns the locale ID for the string table.
+ *
+ * 1.00: D2Lang.0x100010B9 (#10007)
+ * 1.10: D2Lang.0x6FC13DF0 (#10007)
+ * 1.13c: D2Lang.0x6FC09290 (#10009)
+ * 1.14c: Game.0x00522610
+ */
+D2LANG_DLL_DECL D2C_Language STRTABLE_GetLanguage();
