d36a53d2131fc73edb801a34abab8a46e2405faa
Lectem <lectem@gmail.com>
2022-12-30 18:22:53 +0100

    Move .tbl structs out of D2Structs.h

--- source/D2CommonDefinitions/include/D2Structs.h
+++ source/D2CommonDefinitions/include/D2Structs.h
@@ -753,27 +753,6 @@ struct D2SplittedTextStrc
 };
 
 
-struct D2TblHeaderStrc
-{
-	uint16_t nCRC;								//0x00
-	uint16_t nNumElements;						//0x02
-	int32_t nHashTableSize;						//0x04
-	uint8_t nVersion;							//0x08
-	uint32_t dwIndexStart;						//0x09
-	uint32_t dwNumLoops;						//0x0D
-	uint32_t dwFileSize;						//0x11
-};
-
-struct D2TblNodeStrc
-{
-	uint8_t bUsed;								//0x00
-	uint16_t nIndexNo;							//0x01
-	uint32_t dwHashValue;						//0x03
-	uint32_t dwStringOffset;					//0x07
-	uint32_t dwKeyOffset;						//0x0B
-	uint16_t nNameLen;							//0x0F
-};
-
 struct D2TCStackStrc
 {
 	int32_t nPicks;								//0x00
