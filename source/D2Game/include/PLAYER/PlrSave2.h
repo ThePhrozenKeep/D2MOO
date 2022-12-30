e22be345c52676005467bd1cdae974926b1c2321
Lectem <lectem@gmail.com>
2022-12-30 14:59:01 +0100

    Move save structs out of D2Structs.h

--- source/D2Game/include/PLAYER/PlrSave2.h
+++ source/D2Game/include/PLAYER/PlrSave2.h
@@ -3,6 +3,72 @@
 #include "D2Structs.h"
 #include <Units/Units.h>
 
+#pragma pack(1)
+
+struct D2MercSaveDataStrc
+{
+	uint32_t nFlags;							//0x00		//0xAF of D2SaveHeaderStrc
+	uint32_t nSeed;								//0x04		//0xB3
+	uint16_t wName;								//0x08		//0xB7
+	uint16_t nHirelingId;						//0x0A		//0xB9
+	uint32_t nExperience;						//0x0C		//0xBB
+	uint32_t unk0x10[4];						//0x10
+};
+
+struct D2SaveSkillKeyStrc
+{
+	uint16_t nSkill;							//0x00
+	union
+	{
+		struct
+		{
+			uint8_t nKey;						//0x02
+			uint8_t nKeyEx;					//0x03
+		};
+
+		uint16_t nItemSlot;						//0x02
+	};
+};
+
+struct D2SaveHeaderStrc
+{
+	uint32_t dwHeaderMagic;					//0x000
+	uint32_t dwVersion;						//0x004
+	uint32_t dwSize;							//0x008
+	uint32_t dwChecksum;						//0x00C
+	uint32_t dwWeaponSet;						//0x010
+	char szName[16];						//0x014
+	uint32_t dwSaveFlags;						//0x024
+	uint8_t nClass;							//0x028
+	uint8_t nStats;							//0x029
+	uint8_t nSkills;							//0x02A
+	uint8_t nLevel;							//0x02B
+	uint32_t dwCreateTime;						//0x02C
+	uint32_t dwLasTime;						//0x030
+	uint32_t dwPlayTime;						//0x034
+	D2SaveSkillKeyStrc SkillKeys[16];		//0x038
+	int16_t nLeftSkillId;					//0x78
+	int16_t nLeftSkillItemIndex;			//0x7A
+	int16_t nRightSkillId;					//0x7C
+	int16_t nRightSkillItemIndex;			//0x7E
+	int16_t nSwitchLeftSkillId;				//0x80
+	int16_t nSwitchLeftSkillItemIndex;		//0x82
+	int16_t nSwitchRightSkillId;			//0x84
+	int16_t nSwitchRightSkillItemIndex;		//0x86
+	uint8_t nComponent[16];					//0x088
+	uint8_t nCompColor[16];					//0x098
+	uint8_t nTown[3];							//0x0A8
+	uint32_t dwMapSeed;						//0x0AB
+	D2MercSaveDataStrc MercSaveData;		//0x0AF
+	uint8_t nSaveField;						//0x0CF
+	uint32_t dwLastLevel;						//0x0D0
+	uint32_t dwLastTown;						//0x0D4
+	uint8_t nLastDifficulty;					//0x0D8
+
+	char unk0xD9[118];
+};
+
+#pragma pack()
 
 //D2Game.0x6FC8CCD0
 void __fastcall PLRSAVE2_WriteMercData(D2GameStrc* pGame, D2UnitStrc* pPlayer, D2SaveHeaderStrc* pMercData);
