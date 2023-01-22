fd40159aa03b06490cc0e016dd1879c5e12bdfc5
Lectem <lectem@gmail.com>
2023-01-22 20:24:43 +0100

    D2Net: Remove unneeded D2FUNC_DLL_STUB

--- source/D2Net/src/DllMain.cpp
+++ source/D2Net/src/DllMain.cpp
@@ -2,15 +2,7 @@
 
 #include <cstdint>
 
-#include <D2Dll.h>
-
-
-#undef D2FUNC_DLL
-#define D2FUNC_DLL D2FUNC_DLL_STUB
-
-
 extern int32_t dword_6FC0B264;
-
 
 //D2Net.0x6FC01B40
 BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
