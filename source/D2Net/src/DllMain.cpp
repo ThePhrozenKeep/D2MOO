a0c2f5ee0f754a2c09068857078b6b8914e46f7b
Lectem <lectem@gmail.com>
2023-01-22 20:23:40 +0100

    D2Net: put back the dll main switch

--- source/D2Net/src/DllMain.cpp
+++ source/D2Net/src/DllMain.cpp
@@ -15,9 +15,17 @@ extern int32_t dword_6FC0B264;
 //D2Net.0x6FC01B40
 BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	if (fdwReason == DLL_PROCESS_ATTACH)
+	switch (fdwReason)
 	{
+	case DLL_PROCESS_ATTACH:
 		dword_6FC0B264 = 0;
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
 	}
 
 	return TRUE;
