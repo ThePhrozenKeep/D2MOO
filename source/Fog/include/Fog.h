4600df95f8e1c28b42e7ca3a078115c3eddcb509
Lectem <lectem@gmail.com>
2023-12-02 21:21:17 +0100

    Finish reversing AsyncData structs

--- source/Fog/include/Fog.h
+++ source/Fog/include/Fog.h
@@ -106,40 +106,42 @@ enum D2C_AsyncDataPriority
 
 enum D2C_AsyncDataFlags : uint32_t
 {
-	ASYNC_DATA_FLAGS_LOADED = 0x01,
+	ASYNC_DATA_FLAGS_LOADED                    = 0x1,
+	ASYNC_DATA_FLAGS_OWNS_BUFFER               = 0x2,
+	ASYNC_DATA_FLAGS_POSTPONED_PRIORITY_CHANGE = 0x8,
 
 	ASYNC_DATA_FLAGS_DEBUG_TAG      = 0x41737960,
 	ASYNC_DATA_FLAGS_DEBUG_TAG_MASK = 0xFFFFFFF0,
 };
 
-struct AsyncDataLoadFileCallback
+struct AsyncDataLoadFileCallback																			   // sizeof(0x1C)
 {
-	DWORD(__stdcall* callback)(int a1, int a3, int C);
-	DWORD userData[6];
+	DWORD(__stdcall* callback)(void* pFileData, size_t nDataLength, AsyncDataLoadFileCallback* pCallbackData); // 0x00
+	DWORD userData[4];																						   // 0x04
+	void* pData;																							   // 0x14
+	size_t nFileSize;																						   // 0x18
 };
 
-struct AsyncDataEventSlot;
-
-struct AsyncData
+struct AsyncData							 // sizeof(0x54)
 {
-	HSFILE hFile;
-	DWORD pMemPool;
-	OVERLAPPED tOverlapped;
-	AsyncDataLoadFileCallback ayncOpDesc;
-	AsyncDataEventSlot* pEventSlot;
-	void* pBuffer;
-	DWORD nFileSize;
-	DWORD nPriority; // D2C_AsyncDataPriority
-	DWORD dwFlags;
-	DWORD dword4C; // Also storing priority ?
-	DWORD dword50;
+	HSFILE hFile;							 // 0x00
+	void* pMemPool;							 // 0x04
+	OVERLAPPED tOverlapped;					 // 0x08
+	AsyncDataLoadFileCallback pAsyncOpDesc;	 // 0x1C
+	struct AsyncDataEventSlot* pEventSlot;	 // 0x38
+	void* pBuffer;							 // 0x3C
+	uint32_t nFileSize;						 // 0x40
+	uint32_t nPriority;						 // 0x44 D2C_AsyncDataPriority
+	uint32_t nFlags;						 // 0x48
+	uint32_t nPreviousPriority;				 // 0x4C
+	AsyncData* pNextPostponedPriorityChange; // 0x50
 };
 
-struct AsyncDataEventSlot
+struct AsyncDataEventSlot  // sizeof(0x0C)
 {
-	HANDLE hEvent;
-	DWORD dw04;
-	AsyncData* pAsyncOp;
+	HANDLE hEvent;		   // 0x00
+	DWORD bSlotAvailable;  // 0x04
+	AsyncData* pAsyncOp;   // 0x08
 };
 
 
