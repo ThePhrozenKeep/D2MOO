cccb8d2bbbb91cdce511ad8038c693189cbc1f3a
nooperation <madbrahmin@gmail.com>
2024-02-24 18:14:39 -0500

    Fixed SKILLS_CalculateMasteryBonus so it matches the original code's multiplaction and divion ordering. It uses the "MONSTERUNIQUE_CalculatePercentage" logic inlined, but a dependency on Game from Common would look a bit weird so I just left it as is

--- source/D2Common/src/D2Skills.cpp
+++ source/D2Common/src/D2Skills.cpp
@@ -2659,46 +2659,56 @@ int __stdcall SKILLS_GetMinElemDamage(D2UnitStrc* pUnit, int nSkillId, int nSkil
 //D2Common.0x6FDB29D0
 int __fastcall SKILLS_CalculateMasteryBonus(D2UnitStrc* pUnit, int nElemType, int nSrcDamage)
 {
-	int nPercentage = 0;
+	int statId = 0;
 
 	switch (nElemType)
 	{
 	case ELEMTYPE_FIRE:
-		nPercentage = STATLIST_UnitGetStatValue(pUnit, STAT_PASSIVE_FIRE_MASTERY, 0);
-		if (!nPercentage)
-		{
-			return 0;
-		}
-
-		return nSrcDamage * nPercentage / 100;
+		statId = STAT_PASSIVE_FIRE_MASTERY;
+		break;
 	case ELEMTYPE_LTNG:
-		nPercentage = STATLIST_UnitGetStatValue(pUnit, STAT_PASSIVE_LTNG_MASTERY, 0);
-		if (!nPercentage)
-		{
-			return 0;
-		}
-
-		return nSrcDamage * nPercentage / 100;
+		statId = STAT_PASSIVE_LTNG_MASTERY;
+		break;
 	case ELEMTYPE_COLD:
 	case ELEMTYPE_FREEZE:
-		nPercentage = STATLIST_UnitGetStatValue(pUnit, STAT_PASSIVE_COLD_MASTERY, 0);
-		if (!nPercentage)
-		{
-			return 0;
-		}
-
-		return nSrcDamage * nPercentage / 100;
+		statId = STAT_PASSIVE_COLD_MASTERY;
+		break;
 	case ELEMTYPE_POIS:
-		nPercentage = STATLIST_UnitGetStatValue(pUnit, STAT_PASSIVE_POIS_MASTERY, 0);
-		if (!nPercentage)
-		{
-			return 0;
-		}
-		
-		return nSrcDamage * nPercentage / 100;
+		statId = STAT_PASSIVE_POIS_MASTERY;
+		break;
 	default:
 		return 0;
 	}
+
+	auto nPercentage = STATLIST_UnitGetStatValue(pUnit, statId, 0);
+
+	// NOTE: This is an exact copy of D2Game.0x6FC6AF70 (MONSTERUNIQUE_CalculatePercentage)
+	if (!nPercentage)
+	{
+		return 0;
+	}
+
+	if (nSrcDamage <= 0x100000)
+	{
+		if (nPercentage <= 0x10000)
+		{
+			return nSrcDamage * nPercentage / 100;
+		}
+
+		if ((nPercentage >> 4) >= 100)
+		{
+			return nSrcDamage * (nPercentage / 100);
+		}
+	}
+	else
+	{
+		if ((nSrcDamage >> 4) >= 100)
+		{
+			return nPercentage * (nSrcDamage / 100);
+		}
+	}
+
+	return nSrcDamage * (__int64)nPercentage / 100;
 }
 
 //D2Common.0x6FDB2B00 (#11005)
