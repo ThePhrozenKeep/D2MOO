35f769c3e64ab9736dca96fd0278239407955ef6
nooperation <madbrahmin@gmail.com>
2024-02-24 16:04:32 -0500

    Fix MONSTERUNIQUE_CalculatePercentage order of operations (Division happens before multiply in these two cases. Important for large values)

--- source/D2Game/src/MONSTER/MonsterUnique.cpp
+++ source/D2Game/src/MONSTER/MonsterUnique.cpp
@@ -246,14 +246,14 @@ int32_t __fastcall MONSTERUNIQUE_CalculatePercentage(int32_t a1, int32_t a2, int
 
         if (a3 <= a2 >> 4)
         {
-            return a1 * a2 / a3;
+            return a1 * (a2 / a3);
         }
     }
     else
     {
         if (a3 <= a1 >> 4)
         {
-            return a2 * a1 / a3;
+            return a2 * (a1 / a3);
         }
     }
 
