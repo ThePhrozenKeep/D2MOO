e8201e46bd9459f24ee9fa649f1ed3a9b5dd970d
Lectem <lectem@gmail.com>
2024-07-27 22:57:37 +0200

    Add D2Client_GetPlayerUnit

--- source/D2Client/include/Game/Game.h
+++ source/D2Client/include/Game/Game.h
@@ -1,1 +1,10 @@
 #pragma once  
+
+#include <D2BasicTypes.h>
+
+struct D2UnitStrc;
+
+//1.00 :D2Client.0x10001CA3
+//1.10f:D2Client.0x6FB283D0
+D2UnitStrc* __fastcall D2CLIENT_GetPlayerUnit_6FB283D0();
+
--- source/D2Client/src/Game/Game.cpp
+++ source/D2Client/src/Game/Game.cpp
@@ -1,1 +1,21 @@
 #include <Game/Game.h> 
+
+#include <D2Client.h>
+#include <D2Dll.h>
+
+HMODULE delayedD2CLIENTDllBaseGet()
+{
+	static HMODULE DLLBASE_D2LANG = LoadLibraryA("D2Client.dll");
+	return DLLBASE_D2LANG;
+}
+
+//1.10f:D2Client.0x6FBBC200
+//1.13c:D2Client.0x6FBCBBFC
+//D2UnitStrc* gpPlayerUnit_6FBBC200;
+D2VAR(D2CLIENT, pgpPlayerUnit_6FBBC200, D2UnitStrc*, 0x6FBBC200 - D2ClientImageBase);
+//1.00 :D2Client.0x10001CA3
+//1.10f:D2Client.0x6FB283D0
+D2UnitStrc* __fastcall D2CLIENT_GetPlayerUnit_6FB283D0()
+{
+    return *D2CLIENT_pgpPlayerUnit_6FBBC200;
+}
