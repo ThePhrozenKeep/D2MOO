3c07a5ab8d09127813adeeb8d336822471178c14
Lectem <lectem@gmail.com>
2024-12-08 10:10:00 +0100

    Add D2CLIENT_CELLFILE_GetCellFile and D2CLIENT_CELLFILE_FreeCellFile

--- source/D2Client/include/Core/ARCHIVE.h
+++ source/D2Client/include/Core/ARCHIVE.h
@@ -1,1 +1,10 @@
 #pragma once  
+
+#include <D2Gfx.h>
+
+//1.10f:D2Client.0x6FAA1000
+//1.13c:D2Client.0x6FADB420
+D2CellFileStrc* __fastcall D2CLIENT_CELLFILE_GetCellFile(char* szFilename, BOOL bAllowCompressed);
+
+//1.10f:D2Client.0x6FAA1140
+void D2CLIENT_CELLFILE_FreeCellFile(D2CellFileStrc* pCellFile);
